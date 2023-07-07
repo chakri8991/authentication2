17839771d408200f80339b0edd594824f1742974
chakri <chakravarthi1223@gmail.com>
2023-07-07 09:49:21 +0000

    first commit

---
+++ src/components/Cart/index.js
@@ -0,0 +1,14 @@
+// Write your JS code here
+import './index.css'
+
+const Cart = () => (
+  <div className="not-found-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-img.png"
+      alt="cart"
+      className="not-found-img"
+    />
+  </div>
+)
+
+export default Cart
---
+++ src/components/Products/index.js
@@ -0,0 +1,17 @@
+// Write your JS code here
+
+import './index.css'
+
+const Products = () => (
+  //   <Link to="/products">
+  <div className="not-found-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png"
+      alt="products"
+      className="not-found-img"
+    />
+  </div>
+  //   </Link>
+)
+
+export default Products
